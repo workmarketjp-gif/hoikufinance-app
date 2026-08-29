f1e877d08a2d0138edb5e372aca29abbc167defe
workmarketjp-gif <workmarket.jp@gmail.com>
2026-08-30 10:54:14 +1200

    feat: wire branded login page and logo styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import App from "./App";
+import LoginPage from "./LoginPage";
 import "./styles.css";
+import "./brand-overrides.css";
+import "./login.css";
+
+const isLoginRoute = window.location.pathname === "/login";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      {isLoginRoute ? <LoginPage /> : <App />}
     </BrowserRouter>
   </React.StrictMode>
 );
