258ae9e0cb3adf11824e3294a64198cb6a3068ad
workmarketjp-gif <workmarket.jp@gmail.com>
2026-08-30 12:46:28 +1200

    feat: mount budget and operations enhancements

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,19 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import App from "./App";
 import LoginPage from "./LoginPage";
+import FinanceEnhancements from "./FinanceEnhancements";
 import "./styles.css";
 import "./brand-overrides.css";
 import "./login.css";
+import "./budget-operations.css";
+import "./finance-enhancements.css";
 
 const isLoginRoute = window.location.pathname === "/login";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      {isLoginRoute ? <LoginPage /> : <App />}
+      {isLoginRoute ? <LoginPage /> : <><App /><FinanceEnhancements /></>}
     </BrowserRouter>
   </React.StrictMode>
 );
